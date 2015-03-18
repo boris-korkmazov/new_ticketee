543cd050b2e99b8edabf96a9fbe371ad26c5e9df
boriz <boriskorkmazov1989@gmail.com>
2015-03-18 20:14:22 +0300

    Implemented signing in.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   root "projects#index"
 
+  get "/signin" => "sessions#new"
+
+  post "/signin" => "sessions#create"
+
   resources :projects do
     resources :tickets
   end
