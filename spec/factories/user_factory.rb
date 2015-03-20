efb92eace11e5edce2899f9c052fd4228d17b705
boriz <boriskorkmazov1989@gmail.com>
2015-03-20 20:30:07 +0300

    Added admin flag to user

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -4,5 +4,9 @@ FactoryGirl.define do
     email "sample@example.com"
     password "1234567"
     password_confirmation "1234567"
+
+    factory :admin_user do
+      admin true
+    end
   end
 end
